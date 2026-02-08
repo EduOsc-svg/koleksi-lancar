85a63246bf7a2507eeb25a6c95617f3f72b6e719
EduOsc-svg <t19-0042731048@smkn7-smr.sch.id>
2026-02-08 11:48:50 +0800

    update

--- src/components/print/PrintCoupon8x5.tsx
+++ src/components/print/PrintCoupon8x5.tsx
@@ -179,9 +179,9 @@ export function PrintCoupon8x5({ coupons, contract }: PrintCoupon8x5Props) {
       
       /* KHUSUS: Spacing untuk field yang membutuhkan spasi berbeda */
       .field-spacing-large {
-        margin-top: 3.5px;
+        margin-top: 3px;
         margin-bottom: 3px;
-        padding-top: 1px;
+        padding-top: 1.5px;
         padding-bottom: 1px;
       }
       
