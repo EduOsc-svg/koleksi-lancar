790fa5a4d43716b4da98cc6c286783178c3310a3
EduOsc-svg <t19-0042731048@smkn7-smr.sch.id>
2026-02-10 16:54:02 +0800

    Update

--- configure_git_proxy.sh
+++ configure_git_proxy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Git Proxy Configuration Script
-# Updated on Mon Feb 10 2026 with new proxy IP
+# Generated on Tue Feb 10 04:53:46 PM WITA 2026
 
-echo "🌐 Configuring Git Proxy: http://10.49.21.218:7071"
+echo "🌐 Configuring Git Proxy: http://10.22.54.249:7071"
 
 # Set git proxy configurations
-git config --global http.proxy http://10.49.21.218:7071
-git config --global https.proxy http://10.49.21.218:7071
+git config --global http.proxy http://10.22.54.249:7071
+git config --global https.proxy http://10.22.54.249:7071
 git config --global http.sslVerify false
 git config --global http.timeout 300
 git config --global http.postBuffer 524288000
 
 # Set environment variables
-export http_proxy=http://10.49.21.218:7071
-export https_proxy=http://10.49.21.218:7071
-export HTTP_PROXY=http://10.49.21.218:7071
-export HTTPS_PROXY=http://10.49.21.218:7071
+export http_proxy=http://10.22.54.249:7071
+export https_proxy=http://10.22.54.249:7071
+export HTTP_PROXY=http://10.22.54.249:7071
+export HTTPS_PROXY=http://10.22.54.249:7071
 
 echo "✅ Git proxy configured successfully!"
 echo "📋 Configuration:"
