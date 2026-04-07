8a4113ce99a2bd60963e339101d6a0f291b3c710
EduOsc-svg <t19-0042731048@smkn7-smr.sch.id>
2026-04-07 17:15:00 +0800

    Update

--- configure_git_proxy.py
+++ configure_git_proxy.py
@@ -166,7 +166,7 @@ def main():
     print("=" * 50)
     
     # Default proxy configuration
-    proxy_ip = "10.143.233.160"
+    proxy_ip = "10.57.71.7"
     proxy_port = 7071
     
     # Allow command line arguments
